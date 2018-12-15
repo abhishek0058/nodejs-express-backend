a2fbfde89877d7c13c50ba4322c796d66448ac92
shubham kaurav <shubhamkaurav72@gmail.com>
2018-12-15 17:55:51 +0530

    daily report filter purchese history

--- routes/pool.js
+++ routes/pool.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '123',
     database: 'washing',
     multipleStatements: true,
     connectionLimit: 100
