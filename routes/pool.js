c2371cfbeaa343a86d18e54caea9b249a6c57181
Abhishek Sharma <abhishek0058@gmail.com>
2018-12-20 21:45:18 +0530

    chaning database password

--- routes/pool.js
+++ routes/pool.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 const pool = mysql.createPool({
     host: '139.59.94.159',
     user: 'root',
-    password: '', 
+    password: '123', 
     database: 'washing',
     multipleStatements: true,
     connectionLimit: 100
